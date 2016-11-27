6f3c4555bd45897d895bdf24c7b7aeccbf645a84
Quentin Dejean <quentin@cammy.com>
2016-11-27 13:05:36 +1100

    Added eslint config Added eslint loader to webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,16 +1,30 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path              = require('path');
 
+const modules = {
+  rules: [{
+    test: /\.js$/,
+    loader: 'eslint-loader',
+    exclude: /node_modules/
+  }]
+};
+
+const devServer = {
+  stats: 'errors-only'
+};
+
 const context = path.resolve(__dirname, 'app');
 
 const config = [{
   context: `${context}/src/editor`,
-  devtool: "eval-source-map",
+  devServer,
+  devtool: 'eval-source-map',
   entry: `${context}/src/editor/components/index.js`,
   output: {
     path: `${context}/build`,
     filename: 'bundle.editor.js'
   },
+  module: modules,
   plugins: [
     new HtmlWebpackPlugin({
       template: 'assets/index.html'
@@ -18,8 +32,10 @@ const config = [{
   ]
 }, {
   context: `${context}/src/widget`,
-  devtool: "eval-source-map",
+  devServer,
+  devtool: 'eval-source-map',
   entry: `${context}/src/widget/components/index.js`,
+  module: modules,
   output: {
     path: `${context}/build`,
     filename: 'bundle.widget.js'
